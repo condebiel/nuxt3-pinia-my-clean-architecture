ace3fd2e1fc3293916c336d387b116d49672fee6
condebiel <bielbcm@gmail.com>
2023-03-03 14:02:50 +0100

    fix: Change Modal with optional values

--- apps/frontend/src/models/domain/Model.ts
+++ apps/frontend/src/models/domain/Model.ts
@@ -1,12 +1,12 @@
 export type Model = {
-	id: number
+	id: string
 	isModerated: boolean
 	avatar: string
-	age: number
-	eyes: string
+	age?: number
+	eyes?: string
 	name: string
-	gender: string
-	email: string
-	created_at: string
-	tags: string[]
+	gender?: string
+	email?: string
+	created_at?: string
+	tags?: string[]
 }
